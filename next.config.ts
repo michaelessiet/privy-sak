29de174fe74c8dd2ecf7001424501c17dfb2b878
michaelessiet <essiet2002@gmail.com>
2025-04-07 17:58:22 +0100

    fix: switch to turbopack?

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,25 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
+  webpack: (config, { isServer, dev }) => {
+    // Only apply this fix for client-side production builds
+    if (!isServer && !dev) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false, // This tells webpack to provide an empty module
+        net: false,
+        _http_common: false,
+      };
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
-        hostname: 'avatar.vercel.sh',
+        hostname: "avatar.vercel.sh",
       },
     ],
   },
